0ca7d5d057d7db7fd682f636feffafd8c8b33e85
faridkazemi <farid.kazemi63@gmail.com>
2025-05-15 19:07:46 +1000

    Just a change

--- UserService/UserService/Function1.cs
+++ UserService/UserService/Function1.cs
@@ -17,7 +17,7 @@ namespace UserService
         [Function("Function1")]
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogInformation("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHeeeeeeeeeyyyyy");
             return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
