332e6f60e25a15030440041fd86bfd5acde43ab5
faridkazemi <farid.kazemi63@gmail.com>
2025-05-15 20:26:31 +1000

    Added service bus publisher

--- UserService/UserService/Function1.cs
+++ UserService/UserService/Function1.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace UserService
 {
@@ -9,16 +11,67 @@ namespace UserService
     {
         private readonly ILogger<Function1> _logger;
 
+        public static List<User> Users = new List<User>();
+
         public Function1(ILogger<Function1> logger)
         {
             _logger = logger;
+
+            Users.Add(new User { Email = "abc@uuu.com", Id = Guid.NewGuid(), Name = "Farid" });
+            Users.Add(new User { Email = "abc2@uuu.com", Id = Guid.NewGuid(), Name = "Farid2" });
+            Users.Add(new User { Email = "abc3@uuu.com", Id = Guid.NewGuid(), Name = "Farid3" });
+            Users.Add(new User { Email = "abc4@uuu.com", Id = Guid.NewGuid(), Name = "Farid4" });
         }
 
-        [Function("Function1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        [Function("Users")]
+        public IActionResult GetUsers([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
         {
             _logger.LogInformation("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHeeeeeeeeeyyyyy");
-            return new OkObjectResult("Welcome to Azure Functions!");
+
+
+            return new OkObjectResult(Users);
         }
+
+        [Function("User")]
+        public async Task<IActionResult> SaveUser([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+        {
+            _logger.LogInformation("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHeeeeeeeeeyyyyy");
+            try
+            {
+                
+                var user = await req.ReadFromJsonAsync<User>();
+
+                Users.Add(user);
+
+                var serviceBusPublisher = new ServiceBusPublisher();
+
+                var userJson = JsonSerializer.Serialize(user);
+                
+                await serviceBusPublisher.PublishMessage(userJson);
+
+                return new OkResult();
+            }
+            catch(Exception e)
+            {
+                _logger.LogError(e.Message);
+                throw;
+            }
+
+
+            
+        }
+    }
+
+    public class User
+    {
+        private Guid _id;
+
+        public Guid Id
+        {
+            get => _id;
+            set => _id = value == Guid.Empty ? Guid.NewGuid() : value;
+        }
+        public string Name { get; set; }
+        public string Email { get; set; }
     }
 }
---
+++ UserService/UserService/ServiceBusPublisher.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService
+{
+    public class ServiceBusPublisher
+    {
+        public ServiceBusPublisher() { }
+
+        public async Task PublishMessage(string messgae)
+        {
+            string fullyQualifiedNamespace = "mehrfarservicebus.servicebus.windows.net";
+            string queueName = "pre-process-notifications";
+
+            // Use DefaultAzureCredential (uses managed identity in Azure)
+            var client = new ServiceBusClient(fullyQualifiedNamespace, new DefaultAzureCredential());
+
+            var sender = client.CreateSender(queueName);
+
+            var message = new ServiceBusMessage(messgae);
+
+            await sender.SendMessageAsync(message);
+
+            Console.WriteLine("Message sent with managed identity.");
+        }
+    }
+}
